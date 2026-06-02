046123f9ba50128556c4cba34b10385fb3772743
Ahnaf <mohammadahnaf123@gmail.com>
2026-06-02 18:46:52 +0400

    Fix imports for payments and inventory modules inside Returns Module

--- src/modules/returns/returns.module.ts
+++ src/modules/returns/returns.module.ts
@@ -5,9 +5,11 @@ import { OrdersModule } from "../orders/orders.module";
 import { ShipmentsModule } from "../shipments/shipments.module";
 import { ReturnsController } from "./returns.controller";
 import { ReturnsService } from "./returns.service";
+import { PaymentsModule } from "../payments/payments.module";
+import { InventoryModule } from "../inventory/inventory.module";
 
 @Module({
-  imports: [OrdersModule, ShipmentsModule],
+  imports: [OrdersModule, ShipmentsModule, PaymentsModule, InventoryModule],
   controllers: [ReturnsController],
   providers: [ReturnsService, PrismaService],
   exports: [ReturnsService],
